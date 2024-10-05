21c028a00232ee14477e1281826fd5f3b5ce3efc
Grady11-G <gkickz21@yahoo.com>
2024-10-05 11:10:18 -0700

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,1 +1,19 @@
+const workbox = require ("workbox-build");
 
+workbox.generateSW({
+cacheId: "pwa_example", 
+  globDirectory : "./",
+  globPatterns:[
+    "**/*.{css|js}"
+    ],
+  swDest: "./sw.js",
+  runTimeCoaching: [
+    {
+      urlPatterson: /\.(?:html|htm|xml)$/,
+      handler: "staleWhileRevalidate",
+      options: {
+        cacheName:"markup",
+        expiration:{
+          maxAgeSeconds: 1800
+        }
+      })
